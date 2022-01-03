363376b4a17659b61aaee3195022c1474e093004
westbrookmd <71563811+westbrookmd@users.noreply.github.com>
2022-01-03 17:36:01 -0500

    Adds Windows Audio Control and Media Playback Control
    Windows Audio Control is added using the .NET Core Audio API.
    
    Media Playback Control is currently being done by sending media keys (after importing user32 dll).

---
+++ MediaDock/WindowsAudio/Core.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsAudio
+{
+    public class Core
+    {
+        //https://stackoverflow.com/a/41534820
+        // controlling media keys
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern void keybd_event(byte virtualKey, byte scanCode, uint flags, IntPtr extraInfo);
+        public const int VK_MEDIA_NEXT_TRACK = 0xB0;
+        public const int VK_MEDIA_PLAY_PAUSE = 0xB3;
+        public const int VK_MEDIA_PREV_TRACK = 0xB1;
+        public const int KEYEVENTF_EXTENTEDKEY = 1;
+        public const int KEYEVENTF_KEYUP = 0;
+
+        public static void Previous()
+        {
+            //previous song
+            keybd_event(VK_MEDIA_PREV_TRACK, 0, KEYEVENTF_EXTENTEDKEY, IntPtr.Zero);
+        }
+
+        public static void Next()
+        {
+            //next song
+            keybd_event(VK_MEDIA_NEXT_TRACK, 0, KEYEVENTF_EXTENTEDKEY, IntPtr.Zero);
+        }
+
+        public static void PausePlay()
+        {
+            //pause or play music
+            keybd_event(VK_MEDIA_PLAY_PAUSE, 0, KEYEVENTF_EXTENTEDKEY, IntPtr.Zero);
+        }
+
+        public static void ToggleMute()
+        {
+            AudioManager.ToggleMasterVolumeMute();
+        }
+
+        
+
+
+    }
+}
