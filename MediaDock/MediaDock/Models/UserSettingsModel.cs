624a715e37e4fa1cf810e524b2f7219f90bd5480
westbrookmd <westbrookmd@email.wofford.edu>
2022-01-14 23:41:22 -0500

    Create Settings Model
    Adds the settings model.

---
+++ MediaDock/MediaDock/Models/UserSettingsModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MediaDock.Models
+{
+    public class UserSettingsModel
+    {
+        public bool WindowIsAlwaysOnTop { get; set; }
+        public WindowStartupLocation WindowStartupLocation { get; set; }
+        public ResizeMode WindowResizeMode { get; set; }
+        public float VolumeSliderUpdateInterval { get; set; }
+    }
+}
