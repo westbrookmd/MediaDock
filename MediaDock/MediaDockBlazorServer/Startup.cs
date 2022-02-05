5bdb6ec263352c8130d30128a403496f4cf7450e
westbrookmd <westbrookmd1@gmail.com>
2022-02-05 12:36:38 -0500

    Adds Media Control to DI

--- MediaDock/MediaDockBlazorServer/Startup.cs
+++ MediaDock/MediaDockBlazorServer/Startup.cs
@@ -29,6 +29,7 @@ namespace MediaDockBlazorServer
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddScoped<IMediaControlsDataAccess, MediaControlsDataAccess>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
