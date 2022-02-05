7a28952ecdc5ec01fed4bf18ef99cb265f719ef6
westbrookmd <westbrookmd1@gmail.com>
2022-02-05 12:36:13 -0500

    Media Data Access
    Adds a model and data access for media control.
    
    Additionally adds an interface for easy dependency injection.
    
    This should make it easy to add database calls in the future or add different media control access methods.

---
+++ MediaDock/MediaDockBlazorServer/Data/IMediaControlsDataAccess.cs
@@ -0,0 +1,12 @@
+﻿namespace MediaDockBlazorServer.Data
+{
+    public interface IMediaControlsDataAccess
+    {
+        bool isPlaying { get;}
+        float Volume { get; set; }
+        string GetPlayingStatus();
+        float GetVolume();
+        void SetPlayingStatus(bool status);
+        void SetVolume(float volume);
+    }
+}
---
+++ MediaDock/MediaDockBlazorServer/Data/MediaControlsDataAccess.cs
@@ -0,0 +1,42 @@
+﻿namespace MediaDockBlazorServer.Data
+{
+    public class MediaControlsDataAccess : IMediaControlsDataAccess
+    {
+        public bool isPlaying { get; set; }
+
+        public string IsPlayingDisplay
+        {
+            get 
+            {
+                if(isPlaying)
+                {
+                    return "Playing";
+                }
+                else
+                {
+                    return "Paused";
+                }
+                
+            }
+        }
+
+        public float Volume { get; set; }
+
+        public string GetPlayingStatus()
+        {
+            return IsPlayingDisplay;
+        }
+        public void SetPlayingStatus(bool status)
+        {
+            isPlaying = status;
+        }
+        public float GetVolume()
+        {
+            return Volume;
+        }
+        public void SetVolume(float volume)
+        {
+            Volume = volume;
+        }
+    }
+}
